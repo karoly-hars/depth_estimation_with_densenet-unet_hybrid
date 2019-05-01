ea880c7626273140b84e7a47fdd1f948d98a7fa4
karoly-hars <harsanyika@gmail.com>
2019-05-01 22:44:53 +0200

    change \' to "

--- net.py
+++ net.py
@@ -8,13 +8,13 @@ import os
 class _DenseLayer(nn.Sequential):
     def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
         super(_DenseLayer, self).__init__()
-        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
-        self.add_module('relu1', nn.ReLU(inplace=True)),
-        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
+        self.add_module("norm1", nn.BatchNorm2d(num_input_features)),
+        self.add_module("relu1", nn.ReLU(inplace=True)),
+        self.add_module("conv1", nn.Conv2d(num_input_features, bn_size *
                         growth_rate, kernel_size=1, stride=1, bias=False)),
-        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
-        self.add_module('relu2', nn.ReLU(inplace=True)),
-        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+        self.add_module("norm2", nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module("relu2", nn.ReLU(inplace=True)),
+        self.add_module("conv2", nn.Conv2d(bn_size * growth_rate, growth_rate,
                         kernel_size=3, stride=1, padding=1, bias=False)),
         self.drop_rate = drop_rate
 
@@ -30,17 +30,17 @@ class _DenseBlock(nn.Sequential):
         super(_DenseBlock, self).__init__()
         for i in range(num_layers):
             layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, drop_rate)
-            self.add_module('denselayer%d' % (i + 1), layer)
+            self.add_module("denselayer%d" % (i + 1), layer)
 
 
 class _Transition(nn.Sequential):
     def __init__(self, num_input_features, num_output_features):
         super(_Transition, self).__init__()
-        self.add_module('norm', nn.BatchNorm2d(num_input_features))
-        self.add_module('relu', nn.ReLU(inplace=True))
-        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+        self.add_module("norm", nn.BatchNorm2d(num_input_features))
+        self.add_module("relu", nn.ReLU(inplace=True))
+        self.add_module("conv", nn.Conv2d(num_input_features, num_output_features,
                                           kernel_size=1, stride=1, bias=False))
-        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+        self.add_module("pool", nn.AvgPool2d(kernel_size=2, stride=2))
 
 
 class UpProjection(nn.Module):
@@ -60,7 +60,7 @@ class UpProjection(nn.Module):
         self.bn2 = nn.BatchNorm2d(num_output_features)
 
     def forward(self, x, new_size):
-        x = F.interpolate(x, size=new_size, mode='bilinear', align_corners=False)
+        x = F.interpolate(x, size=new_size, mode="bilinear", align_corners=False)
         branch1 = self.relu(self.bn1(self.conv1(x)))
         branch1 = self.bn1_2(self.conv1_2(branch1))
         
@@ -102,10 +102,10 @@ class DensenetUnetHybrid(nn.Module):
 
         # First convolution
         self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+            ("conv0", nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ("norm0", nn.BatchNorm2d(num_init_features)),
+            ("relu0", nn.ReLU(inplace=True)),
+            ("pool0", nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
         ]))
 
         # Each denseblock
@@ -113,15 +113,15 @@ class DensenetUnetHybrid(nn.Module):
         for i, num_layers in enumerate(block_config):
             block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
                                 bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
-            self.features.add_module('denseblock%d' % (i + 1), block)
+            self.features.add_module("denseblock%d" % (i + 1), block)
             num_features = num_features + num_layers * growth_rate
             if i != len(block_config) - 1:
                 trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
+                self.features.add_module("transition%d" % (i + 1), trans)
                 num_features = num_features // 2
 
         # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+        self.features.add_module("norm5", nn.BatchNorm2d(num_features))
 
         # Conv2
         self.conv2 = nn.Conv2d(1664, 832, 1, bias=True)
@@ -133,7 +133,7 @@ class DensenetUnetHybrid(nn.Module):
         self.up3 = UpProjection(208, 104)
         self.up4 = UpProjection(104, 52)
                 
-        ''' padding + concat for unet stuff '''
+        # padding + concat for unet stuff
         self.con_conv1 = ConConv(640, 416, 416)
         self.con_conv2 = ConConv(256, 208, 208) 
         self.con_conv3 = ConConv(128, 104, 104) 
@@ -189,7 +189,7 @@ class DensenetUnetHybrid(nn.Module):
         x = self.conv2(block4)
         x = self.bn2(x)
         
-        ''' up project part'''
+        # up project part
         x = self.up1(x, [block3.size(2), block3.size(3)])
         x = self.con_conv1(x, block3)
         
@@ -209,18 +209,18 @@ class DensenetUnetHybrid(nn.Module):
         return x
 
 
-def get_model(load_path='DE_densenet.model', use_gpu=True):
+def get_model(load_path="DE_densenet.model", use_gpu=True):
     model = DensenetUnetHybrid(num_init_features=64, growth_rate=32,
                                block_config=(6, 12, 32, 32), drop_rate=0)
 
     # download the weights in case they are not present
     if not os.path.exists(load_path):
-        print('Downloading model weights...')
+        print("Downloading model weights...")
         os.system("wget https://www.dropbox.com/s/jf4elm14ts1da1n/DE_densenet.model")
-        print('Done.')
+        print("Done.")
     if use_gpu:
         model.load_state_dict(torch.load(load_path))
         model = model.cuda()
     else:
-        model.load_state_dict(torch.load(load_path, map_location='cpu'))
+        model.load_state_dict(torch.load(load_path, map_location="cpu"))
     return model
